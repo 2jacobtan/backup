5dce138514cd80da982600bf8004a56d56820e09
Jacob Tan En <jacob.tan.en@gmail.com>
2020-09-09 17:34:38 +0800

    Add environment_variables.zsh

---
+++ environment_variables.zsh
@@ -0,0 +1,7 @@
+[ -f "/home/jt2/.ghcup/env" ] && source "/home/jt2/.ghcup/env" # ghcup-env
+
+# Haskell Stack installer told me to add this.
+export PATH="/home/jt2/.local/bin:$PATH"
+
+# use vim as default editor (e.g. when git looks for one)
+export EDITOR=/usr/bin/vim
